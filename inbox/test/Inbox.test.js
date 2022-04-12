fb4f3fe91e7b0f78a699922e54e91b3b345ecf54
Massimiliano Bruni <massimiliano.bruni@hcl.com>
2022-04-12 14:54:06 +0200

    Updated solidity version

--- inbox/test/Inbox.test.js
+++ inbox/test/Inbox.test.js
@@ -2,7 +2,7 @@ const assert = require("assert");
 const ganache = require("ganache-cli");
 const Web3 = require("web3");
 const web3 = new Web3(ganache.provider());
-const { interface, bytecode } = require("../compile");
+const { abi, evm } = require("../compile");
 
 let accounts;
 let inbox;
@@ -11,10 +11,9 @@ const INITIAL_STRING = "Hi there!";
 beforeEach(async () => {
   // Get a list of all accounts
   accounts = await web3.eth.getAccounts();
-  // Use one of those accounts to deploy the contract
-  inbox = await new web3.eth.Contract(JSON.parse(interface))
+  inbox = await new web3.eth.Contract(abi)
     .deploy({
-      data: bytecode,
+      data: evm.bytecode.object,
       arguments: [INITIAL_STRING],
     })
     .send({ from: accounts[0], gas: "1000000" });
