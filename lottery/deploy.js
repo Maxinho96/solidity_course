82479ccd2920758324e6ef89070d3a7bb87f726e
Massimiliano Bruni <massimiliano.bruni@icloud.com>
2022-04-30 16:35:40 +0200

    Added lottery contract

---
+++ lottery/deploy.js
@@ -0,0 +1,24 @@
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const Web3 = require("web3");
+const { abi, evm } = require("./compile");
+
+const provider = new HDWalletProvider(
+  "sense blush cream flag torch opera trophy cable museum narrow autumn oil",
+  "https://ropsten.infura.io/v3/c3f1c56221dd4be29520f65f4bec65e2"
+);
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  console.log("Attempting to deploy from account", accounts[0]);
+
+  const result = await new web3.eth.Contract(abi)
+    .deploy({ data: evm.bytecode.object, arguments: ["Hi there!"] })
+    .send({ gas: "1000000", from: accounts[0] });
+
+  console.log("Contract deployed to", result.options.address);
+  provider.engine.stop();
+};
+
+deploy();
